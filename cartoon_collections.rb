79c4792ce85f2857b1a6e082f07d2b1491740d28
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-09 15:43:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,8 @@ def long_planeteer_calls(array)# code an argument here
   # Your code here
   i = 0
   if array.any? {|i| i.length > 4}
-    
-  
+    return true
+  end
 end
 
 def find_the_cheese# code an argument here
