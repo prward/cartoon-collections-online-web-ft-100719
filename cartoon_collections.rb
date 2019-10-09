7b5bdeedf4e5ceff760c897c0c5e4e4c28b27c86
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-09 15:35:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
-def roll_call_dwarves(dwarf)# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
   i = 0
-  while i < dwarf.length
+  while i < array.length
   puts "#{i + 1}. #{dwarf[i}"
   i += 1
 end
